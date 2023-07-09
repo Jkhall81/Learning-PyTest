fc904a39f8b43d7c3da90bc391383ab75ee3e9b9
Jkhall81 <jason.kei.hall@gmail.com>
2023-07-08 18:19:54 -0700

    made the funcitons a little more complicated, and added new tests.  Also added more to the readme.  This looks a little better now.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 def round6(num: float) -> int:
-    """This function has a bug in it."""
+    """This function rounds a number to the nearest integer."""
     return int(num + 0.6)
 
 
+def add(a: int, b: int) -> int:
+    """This function adds two numbers."""
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """This function multiplies two numbers."""
+    return a * b
+
+
 def main():
-    result = round6(3.5)
-    return result
+    result1 = round6(3.5)
+    print("Rounded result:", result1)
+
+    result2 = add(4, 7)
+    print("Addition result:", result2)
+
+    result3 = multiply(5, 3)
+    print("Multiplication result:", result3)
 
 
 if __name__ == '__main__':
     main()
-
-
-print(round6(4.5))
--- test_round6.py
+++ test_round6.py
@@ -1,13 +1,24 @@
+import main
 import pytest
-from main import round6
 
 
-def test_round6_rounds_up():
-    assert round6(9.7) == 10
+def test_round6():
+    assert main.round6(3.5) == 4
+    assert main.round6(4.2) == 4
+    assert main.round6(5.8) == 6
 
 
-def test_round6_rounds_down():
-    assert round6(8.5) == 8
+def test_add():
+    assert main.add(2, 3) == 5
+    assert main.add(-1, 1) == 0
+    assert main.add(0, 0) == 0
 
 
-pytest.main()
+def test_multiply():
+    assert main.multiply(2, 3) == 6
+    assert main.multiply(-1, 5) == -5
+    assert main.multiply(4, 0) == 0
+
+
+if __name__ == '__main__':
+    pytest.main()
